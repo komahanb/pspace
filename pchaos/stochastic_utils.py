f9a3e879e99b272ae9bd9a5147dd9dcc80e8e762
Komahan Boopathy <komahan.cool@gmail.com>
2019-05-21 17:05:39 -0400

    using counters for basisterm wise degree map

--- pchaos/stochastic_utils.py
+++ pchaos/stochastic_utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from collections import Counter
 
 ## def get_tensor_quadrature_index(k, param_nqpts_map):
 ##     pidx = {}
@@ -19,12 +19,12 @@ import numpy as np
         
 ##     return 
 
-def nqpts(deg):
+def nqpts(pdeg):
     """
     Return the number of quadrature points necessary to integrate the
     monomial of degree deg.
     """
-    return 1 + deg/2 #max(deg/2+1,1)
+    return 1 + pdeg/2 #max(deg/2+1,1)
 
 def tensor_indices(phdmap):
     """
@@ -46,23 +46,23 @@ def tensor_indices(phdmap):
     if num_vars == 1:
         ctr = 0
         for k0 in range(pdegs[0]):
-            idx[k0].append({pids[0]:k0}) # add one element tuple to map
+            idx[k0].append(Counter({pids[0]:k0})) # add one element tuple to map
             ctr += 1
     elif num_vars == 2:
         ctr = 0
         for k0 in range(pdegs[0]):
             for k1 in range(pdegs[1]):
-                idx[k0+k1].append({pids[0]:k0,
-                                   pids[1]:k1}) # add two element tuple to map
+                idx[k0+k1].append(Counter({pids[0]:k0,
+                                           pids[1]:k1})) # add two element tuple to map
                 ctr += 1
     elif num_vars == 3:
         ctr = 0
         for k0 in range(pdegs[0]):
             for k1 in range(pdegs[1]):
                 for k2 in range(pdegs[2]):
-                    idx[k0+k1+k2].append({pids[0]:k0,
-                                          pids[1]:k1,
-                                          pids[2]:k2}) # add three element tuple to map
+                    idx[k0+k1+k2].append(Counter({pids[0]:k0,
+                                                  pids[1]:k1,
+                                                  pids[2]:k2})) # add three element tuple to map
                     ctr += 1
     elif num_vars == 4:
         ctr = 0
@@ -70,10 +70,10 @@ def tensor_indices(phdmap):
             for k1 in range(pdegs[1]):
                 for k2 in range(pdegs[2]):
                     for k3 in range(pdegs[3]):
-                        idx[k0+k1+k2+k3].append({pids[0]:k0,
-                                                 pids[1]:k1,
-                                                 pids[2]:k2,
-                                                 pids[3]:k3}) # add four element tuple to map
+                        idx[k0+k1+k2+k3].append(Counter({pids[0]:k0,
+                                                         pids[1]:k1,
+                                                         pids[2]:k2,
+                                                         pids[3]:k3})) # add four element tuple to map
                         ctr += 1 
     elif num_vars == 5:
         ctr = 0
@@ -82,11 +82,11 @@ def tensor_indices(phdmap):
                 for k2 in range(pdegs[2]):
                     for k3 in range(pdegs[3]):
                         for k4 in range(pdegs[4]):
-                            idx[k0+k1+k2+k3+k4].append({pids[0]:k0,
-                                                 pids[1]:k1,
-                                                 pids[2]:k2,
-                                                 pids[3]:k3,
-                                                 pids[4]:k4}) # add five element tuple to map
+                            idx[k0+k1+k2+k3+k4].append(Counter({pids[0]:k0,
+                                                                pids[1]:k1,
+                                                                pids[2]:k2,
+                                                                pids[3]:k3,
+                                                                pids[4]:k4})) # add five element tuple to map
                             ctr += 1
     else:
         print 'fix implementation for more elements in tuple'
