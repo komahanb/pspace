822dc42fc0bcc1167fb4a13d2a371d907b7123d0
Komahan Boopathy <komahan.cool@gmail.com>
2019-06-09 16:48:06 -0400

    fixed assertions

--- pchaos/parameters.py
+++ pchaos/parameters.py
@@ -14,19 +14,7 @@ from orthogonal_polynomials import unit_hermite as Hhat
 from orthogonal_polynomials import unit_legendre as Phat
 from orthogonal_polynomials import unit_laguerre as Lhat
 from plotter import plot_jacobian
-
-def index(ii):
-    return ii
-    if ii == 0:
-        return ii
-    if ii == 1:
-        return ii
-    if ii == 2:
-        return 3
-    if ii == 3:
-        return 2    
-    return ii
-
+    
 ## TODO
 # Parameters are Monomials
 # Deterministic parameters are constant monomials (degree 0)
@@ -172,7 +160,7 @@ class ExponentialParameter(Parameter):
     
             # assert if weights don't add up to unity
             eps = np.finfo(np.float64).eps
-            #assert((1.0 - eps <= np.sum(w) <= 1.0 + eps) == True)
+            assert((1.0 - 2.0*eps <= np.sum(w) <= 1.0 + 2.0*eps) == True)
     
             # Return quadrature point in standard space as well
             z = xi # (y-mu)/beta
@@ -220,7 +208,7 @@ class NormalParameter(Parameter):
     
             # assert if weights don't add up to unity
             eps = np.finfo(np.float64).eps
-            assert((1.0 - eps <= np.sum(w) <= 1.0 + eps) == True)
+            assert((1.0 - 2.0*eps <= np.sum(w) <= 1.0 + 2.0*eps) == True)
     
             # Return quadrature point in standard space as well
             z = (y-mu)/sigma
@@ -797,10 +785,10 @@ class ParameterContainer:
             # Distribute the residual
             for ii in range(nnodes):
                 # Local indices
-                listart = index(ii)*ndisps
-                liend   = (index(ii)+1)*ndisps                                               
-                gistart = index(ii)*nsdof + i*ndisps
-                giend   = index(ii)*nsdof + (i+1)*ndisps
+                listart = ii*ndisps
+                liend   = (ii+1)*ndisps                                               
+                gistart = ii*nsdof + i*ndisps
+                giend   = ii*nsdof + (i+1)*ndisps
 
                 # Place in global residul array node by node
                 #print(gistart, giend, listart, liend)
@@ -885,15 +873,15 @@ class ParameterContainer:
                         for jj in range(nnodes):
 
                             # Local indices
-                            listart = index(ii)*ndisps
-                            liend   = (index(ii)+1)*ndisps                    
-                            ljstart = index(jj)*ndisps
-                            ljend   = (index(jj)+1)*ndisps
+                            listart = ii*ndisps
+                            liend   = (ii+1)*ndisps                    
+                            ljstart = jj*ndisps
+                            ljend   = (jj+1)*ndisps
                             
-                            gistart = index(ii)*nsdof + i*ndisps
-                            giend   = index(ii)*nsdof + (i+1)*ndisps
-                            gjstart = index(jj)*nsdof + j*ndisps
-                            gjend   = index(jj)*nsdof + (j+1)*ndisps
+                            gistart = ii*nsdof + i*ndisps
+                            giend   = ii*nsdof + (i+1)*ndisps
+                            gjstart = jj*nsdof + j*ndisps
+                            gjend   = jj*nsdof + (j+1)*ndisps
                             
                             if i == j:
                                 J[gistart:giend, gjstart:gjend] += jtmp[listart:liend, ljstart:ljend]
@@ -901,7 +889,7 @@ class ParameterContainer:
                                 J[gistart:giend, gjstart:gjend] += jtmp[listart:liend, ljstart:ljend]
                                 J[gjstart:gjend, gistart:giend] += jtmp[listart:liend, ljstart:ljend]
 
-        # plot_jacobian(J, 'stochatic-element-block.pdf', normalize= True, precision=1.0e-6)
+        plot_jacobian(J, 'stochatic-element-block.pdf', normalize=False, precision=1.0e-6)
         
         return
 
@@ -956,10 +944,10 @@ class ParameterContainer:
             # Distribute values
             for ii in range(nnodes):               
                 # Local indices
-                listart = index(ii)*ndisps
-                liend   = (index(ii)+1)*ndisps                                               
-                gistart = index(ii)*nsdof + k*ndisps
-                giend   = index(ii)*nsdof + (k+1)*ndisps
+                listart = ii*ndisps
+                liend   = (ii+1)*ndisps                                               
+                gistart = ii*nsdof + k*ndisps
+                giend   = ii*nsdof + (k+1)*ndisps
 
                 # Place in initial condition array node after node
                 v[gistart:giend] += utmp[listart:liend]
