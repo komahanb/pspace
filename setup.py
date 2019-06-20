137dda944d92ede00809cb9af1652940e5f6f657
Komahan Boopathy <komahan.cool@gmail.com>
2019-06-20 17:11:54 -0400

    renamed repo and files

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='pchaos',
+    name='pspace',
     version='0.1',
     description='Polynomial chaos package for stochastic galerkin and collocation methods of uncertainty quantification',
     long_description=readme,
     author='Komahan Boopathy',
     author_email='komibuddy@gmail.com',
-    url='https://github.com/komahanb/pchaos',
+    url='https://github.com/komahanb/pspace',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
