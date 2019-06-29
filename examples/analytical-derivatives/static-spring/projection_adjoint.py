3236098bf93a87add3a568a7cc9a6cbe2df1771e
Komahan Boopathy <komahan.cool@gmail.com>
2019-06-29 13:55:08 -0400

    debug adjoint derivaives

--- examples/analytical-derivatives/static-spring/projection_adjoint.py
+++ examples/analytical-derivatives/static-spring/projection_adjoint.py
@@ -26,7 +26,7 @@ class  StochasticSpring:
         self.nterms = self.pc.getNumStochasticBasisTerms()
         self.nsdof = self.nddof*self.nterms
         return
-    
+        
     def solve(self, u, f):
         """
         Fake the linear solve like newton solve with zero initial
@@ -127,10 +127,108 @@ class  StochasticSpring:
         # Solve the linear system 
         U = U - np.linalg.solve(J.T, F)
 
-        print(J)
-        print(F)
         return U
-                
+
+    def adjoint_solve(self, U):
+
+        # Project f and create F
+        dFdQ = np.zeros((self.nsdof))
+        
+        for i in range(self.nterms):
+
+            # Determine num quadrature point required for k-th
+            # projection
+            self.pc.initializeQuadrature(
+                self.pc.getNumQuadraturePointsFromDegree(
+                    self.pc.basistermwise_parameter_degrees[i]
+                    )
+                )
+            
+            # Loop through quadrature points
+            fitmp = 0.0
+            for q in self.pc.quadrature_map.keys():
+                # Quadrature node and weight
+                yq = self.pc.Y(q,'name')
+                wq = self.pc.W(q)
+
+                # Set the paramter values into the element
+                dspr.setStiffness(yq['K'])
+
+                # Create space for fetching deterministic
+                # jacobian, and state vectors that go as input
+                uq = np.zeros((self.nddof))
+                for k in range(self.nterms):
+                    psiky = self.pc.evalOrthoNormalBasis(k,q)
+                    uq[:] += U[k*self.nddof:(k+1)*self.nddof]*psiky
+                            
+                # Evaluate function
+                dfdqq = dspr.dFdq(uq)
+
+                # Project the determinic initial conditions onto the
+                # stochastic basis
+                psiziw = wq*self.pc.evalOrthoNormalBasis(i,q)
+                fitmp += dfdqq*psiziw
+
+            # Set into the force vector
+            dFdQ[i] = fitmp
+
+        # All stochastic parameters are assumed to be of degree 1
+        # (constant terms)
+        dmapf = Counter()
+        for pid in self.pc.parameter_map.keys():
+            dmapf[pid] = 1        
+
+        J = np.zeros((self.nsdof,self.nsdof))
+        for i in range(self.nterms):            
+            imap = self.pc.basistermwise_parameter_degrees[i]
+            
+            for j in range(self.nterms):                
+                jmap = self.pc.basistermwise_parameter_degrees[j]
+                smap = sparse(imap, jmap, dmapf)                
+                if False not in smap.values():                    
+                    dmap = Counter()
+                    dmap.update(imap)
+                    dmap.update(jmap)
+                    dmap.update(dmapf)                    
+                    nqpts_map = self.pc.getNumQuadraturePointsFromDegree(dmap)
+
+                    # Initialize quadrature with number of gauss points
+                    # necessary for i,j-th jacobian entry
+                    self.pc.initializeQuadrature(nqpts_map)
+
+                    jtmp = np.zeros((self.nddof, self.nddof))
+                                    
+                    # Quadrature Loop
+                    for q in self.pc.quadrature_map.keys():
+                        
+                        # Quadrature node and weight
+                        yq = self.pc.Y(q,'name')
+                        wq = self.pc.W(q)
+
+                        # Set the paramter values into the element
+                        dspr.setStiffness(yq['K'])
+  
+                        # Create space for fetching deterministic
+                        # jacobian, and state vectors that go as input
+                        uq = np.zeros((self.nddof))
+                        for k in range(self.nterms):
+                            psiky = self.pc.evalOrthoNormalBasis(k,q)
+                            uq[:] += U[k*self.nddof:(k+1)*self.nddof]*psiky
+                            
+                        # Fetch the deterministic element jacobian matrix
+                        Aq = self.dspr.dRdq(uq)
+                                    
+                        # Project the determinic element jacobian onto the
+                        # stochastic basis and place in the global matrix
+                        psiziw = wq*self.pc.evalOrthoNormalBasis(i,q)
+                        psizjw = self.pc.evalOrthoNormalBasis(j,q)                        
+                        jtmp[:,:] += Aq*psiziw*psizjw
+
+                    # Place the entry into jacobian (transposed)
+                    J[i*self.nddof:(i+1)*self.nddof, j*self.nddof:(j+1)*self.nddof] = jtmp[:, :]
+
+        return - np.linalg.solve(J.T, dFdQ)
+
 def get_func_deriv(k):
     """
     Get the energy value and its derivative for given k value
@@ -149,7 +247,7 @@ def get_func_deriv(k):
 
 # Create random parameters
 pfactory = ParameterFactory()
-K = pfactory.createNormalParameter('K', dict(mu=np.pi/2., sigma=0.1*(np.pi/2.)), 3)
+K = pfactory.createNormalParameter('K', dict(mu=np.pi/2., sigma=0.1*(np.pi/2.)), 8)
 
 # Add random parameters into a container and initialize
 pc = ParameterContainer()
@@ -171,7 +269,10 @@ U = sspr.solve(0, f)
 print("force   :", f)
 print("disp    :", U)
 
+# Decompose f(y) and dfdx(y) in stochastic basis
 E = np.zeros((sspr.nsdof))
+dFdX = np.zeros((sspr.nsdof))
+dRdX = np.zeros((sspr.nsdof))
 for i in range(sspr.nterms):
     # Determine num quadrature point required for k-th
     # projection
@@ -179,9 +280,9 @@ for i in range(sspr.nterms):
         pc.getNumQuadraturePointsFromDegree(
             pc.basistermwise_parameter_degrees[i]
             )
-        )    
+        )
+    
     # Quadrature Loop
-    ftmp = 0
     for q in pc.quadrature_map.keys():                        
         # Quadrature node and weight
         yq = pc.Y(q,'name')
@@ -192,12 +293,14 @@ for i in range(sspr.nterms):
       
         # Create space for fetching deterministic
         # jacobian, and state vectors that go as input
-        uq = np.zeros((1))# dspr.nddof
+        uq = np.zeros((1)) # dspr.nddof
         for k in range(sspr.nterms):
             psiky = pc.evalOrthoNormalBasis(k,q)
             uq[:] += U[k*1:(k+1)*1]*psiky
 
-        fq = dspr.F(uq)
+        fq    = dspr.F(uq)
+        dfdxq = dspr.dFdk(uq)
+        dRdxq = dspr.dRdk(uq)
         
         # Form u for this quadrature point 'y'    
         # Project the determinic element jacobian onto the
@@ -206,29 +309,61 @@ for i in range(sspr.nterms):
         
         # F(q,q(y))*psi(y)*rho(y)
         E[i] += fq*psiziw
+        dFdX[i] += dfdxq*psiziw
+        dRdX[i] += dRdxq*psiziw
 
 fmean = E[0]
 fvar  = np.sum(E[1:]**2)
 fstd  = np.sqrt(fvar)
-print("fmean :", E[0], "fvar :", fvar, "fstd :", fstd) 
+print("E[f]    :", E[0], "V[f]     :", fvar, "S[f] :", fstd) 
+
+EdRdX = dRdX[0]
+VdRdX = np.sum(dRdX[1:]**2)
+SdRdX = np.sqrt(VdRdX)
+print("E[dRdx] :", EdRdX, "V[dRdx] :", VdRdX, "S[dRdx] : :", SdRdX)
+
+EdFdX = dFdX[0]
+VdFdX = np.sum(dFdX[1:]**2)
+SdFdX = np.sqrt(VdFdX)
+print("E[dFdx] :", EdFdX, "V[dFdx] :", VdFdX, "S[dFdx] : :", SdFdX)
+
+lam = sspr.adjoint_solve(U)
+print(lam)
+print(dRdX)
+print(lam*dRdX)
+print(dFdX)
+DFDX = dFdX + lam*dRdX
+fmeanprime = DFDX[0]
+fvarprime  = np.sum(DFDX[1:]**2)
+fstdprime  = np.sqrt(fvarprime)
+print("fmean :", fmean, "fmeanprime", fmeanprime)
+print("fvar  :", fvar , "fvar prime", fvarprime)
+print("fstd  :", fstd , "fstd prime", fstdprime)
+stop
+print("Adjoint DFDx  :", DFDx)
+print("Exact   DFDx  :", -0.5*uhat*uhat)
+
+stop
 
 print("\nk-derivatives... ")
-
 dRdkq = spr.dRdk(uq)
 dFdkq = spr.dFdk(uq)
 
-print("dRdk  :", dRdk)
-print("dFdk  :", dFdk)
+## print("dRdk  :", dRdk)
+## print("dFdk  :", dFdk)
 
-print("\nq-derivatives...")
-dRdq = spr.dRdq(uhat)
-dFdq = spr.dFdq(uhat)
-print("dRdq  :", dRdq)
-print("dFdq  :", dFdq)
+## print("\nq-derivatives...")
+## dRdq = spr.dRdq(uhat)
+## dFdq = spr.dFdq(uhat)
+## print("dRdq  :", dRdq)
+## print("dFdq  :", dFdq)
 
 print("\nadjoint derivative...")
-lam = spr.adjoint_solve(uhat)
+lam = spr.adjoint_solve(U)
 print("lambda:", lam)
+
+stop
+
 DFDx = dFdk + lam*dRdk
 print("Adjoint DFDx  :", DFDx)
 print("Exact   DFDx  :", -0.5*uhat*uhat)
