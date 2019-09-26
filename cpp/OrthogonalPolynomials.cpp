705c96963a798f0f51c6c350adb7ab402ebbc24f
Komahan Boopathy <komahan.cool@gmail.com>
2019-09-26 17:14:42 -0400

    added template orthogonal polynomial class

---
+++ cpp/OrthogonalPolynomials.cpp
@@ -0,0 +1,20 @@
+#include "OrthogonalPolynomials.h"
+
+/*
+  Constructor
+ */
+OrthogonalPolynomials::OrthogonalPolynomials(){}
+
+/*
+  Destructor
+ */
+OrthogonalPolynomials::~OrthogonalPolynomials(){}
+
+/*  
+ */
+double OrthogonalPolynomials::hermite(double z, int d){
+}
+
+double OrthogonalPolynomials::factorial(int n){
+
+}
