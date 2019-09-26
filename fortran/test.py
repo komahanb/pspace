90f3836a96e31b040d0c4082ec376ec3235ab0fb
Komahan Boopathy <komahan.cool@gmail.com>
2019-09-26 11:48:16 -0400

    updated factorial, implemented legendre, speed up tested

--- fortran/test.py
+++ fortran/test.py
@@ -1,6 +1,11 @@
-import pyorthogonal_polynomials as pyortho
-fortho =  pyortho.orthogonal_polynomials
-z = 1.01
-for d in range(45):
-    print(d,fortho.hermite(z,d))
+import forthogonal_polynomials as fortho
+import numpy as np
+
+fortho =  fortho.orthogonal_polynomials
+z = np.random.rand()
+for t in range(10000):
+    for d in range(10):
+        print(d,fortho.hermite(z,d))
+        print(d,fortho.legendre(z,d))
+        print(d,fortho.laguerre(z,d))
 
