9d1f52bb37167af6f6e8eacd9b383611748a4b18
komahan <komahan.cool@gmail.com>
2019-09-27 17:00:25 -0400

    wrapped fortran quadrature in python and tested

--- cpp/test.py
+++ cpp/test.py
@@ -4,10 +4,13 @@ import numpy as np
 poly = cortho.PyOrthogonalPolynomials()
 z = np.random.rand()
 for t in range(1):
+    print 'hermite'
     for d in range(10):
         print(d,poly.hermite(z,d) , poly.unit_hermite(z,d))
+    print 'legendre'
     for d in range(10):
         print(d,poly.legendre(z,d), poly.unit_legendre(z,d))
+    print 'laguerre'
     for d in range(10):
         print(d,poly.laguerre(z,d), poly.unit_laguerre(z,d))
 
