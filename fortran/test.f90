3e31ad6636086142104719cc13d1b5ec59a6a98c
komahan <komahan.cool@gmail.com>
2019-10-02 14:59:38 -0400

    return array for basis

--- fortran/test.f90
+++ fortran/test.f90
@@ -4,8 +4,10 @@ program main
   use uniform_parameter_class
   use exponential_parameter_class
 
-  type(normal_parameter) :: p1
-  type(uniform_parameter) :: p2
+  integer, parameter :: nvars = 3
+  integer            :: pmax(nvars)
+  type(normal_parameter)      :: p1
+  type(uniform_parameter)     :: p2
   type(exponential_parameter) :: p3
 
   real(8) :: z
@@ -16,13 +18,13 @@ program main
   y(2) = 1.1d0
   
   z = 1.1d0  
-
+  
   p1 = normal_parameter(1,1.0d0,1.0d0)
   p2 = uniform_parameter(2,1.0d0,2.0d0)
   p3 = exponential_parameter(3,1.0d0,1.0d0)
 
-  print *, p1 % basis(y, 1), p2 % basis(y, 1), p3 % basis(y, 1)
-  
+  pmax = [2,4,2]
+  call getbasis(nvars, pmax)
   do i = 0, 15
      print *, p1 % basis(z, i), p2 % basis(z, i), p3 % basis(z, i)
   end do
