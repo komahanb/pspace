d79abfcd00933c2137fcf788a7a4257f6daeb20b
Komahan Boopathy <komahan.cool@gmail.com>
2019-10-09 19:17:21 -0400

    adding quadrature helper

---
+++ cpp/QuadratureHelper.cpp
@@ -0,0 +1,9 @@
+#include "QuadratureHelper.h"
+
+QuadratureHelper::QuadratureHelper(){}
+QuadratureHelper::~QuadratureHelper(){}
+
+void QuadratureHelper::tensorProduct(const int nvars,
+                                     const int *nqpts,
+                                     const int **zp, const int **yp, const int **wp,
+                                     int **zz, int **yy, int **ww){}
