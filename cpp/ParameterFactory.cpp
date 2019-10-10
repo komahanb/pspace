0742434a0d8c665bf017bf47d71477ecc1b08b48
Komahan Boopathy <komahan.cool@gmail.com>
2019-10-10 11:59:03 -0400

    adding factory prototype

---
+++ cpp/ParameterFactory.cpp
@@ -0,0 +1,7 @@
+#include"ParameterFactory.h"
+
+ParameterFactory::ParameterFactory(){}
+ParameterFactory::~ParameterFactory(){}
+void ParameterFactory::createNormalParameter(double mu, double sigma){}
+void ParameterFactory::createUniformParameter(double a, double b){}
+void ParameterFactory::createExponentialParameter(double mu, double beta){}
