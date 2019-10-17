4a92dfc3639305925e47b751b92c35f27cea5cf9
Komahan Boopathy <komahan.cool@gmail.com>
2019-10-17 11:36:18 -0400

    added basis implementation for upto 5 variables

--- cpp/BasisHelper.cpp
+++ cpp/BasisHelper.cpp
@@ -51,6 +51,37 @@ void BasisHelper::sparse(const int nvars,
 
 void BasisHelper::univariateBasisDegrees(const int nvars, const int *pmax, 
                                          int *nindices, int **indx){
+  // Number of terms from tensor product
+  int nterms = 1;
+  for (int i = 0; i < nvars; i++){
+    nterms *= 1 + pmax[i];
+  }
+
+  int num_total_degrees = 1;
+  for (int i = 0; i < nvars; i++){
+    num_total_degrees += pmax[i];
+  }
+
+  // Create a map of empty array lists
+  std::map<int,ArrayList*> dmap;
+  for (int k = 0; k < num_total_degrees; k++){
+    dmap.insert(pair<int, ArrayList*>(k, new ArrayList(nterms, nvars)));
+  }
+
+  // Add degree wise tuples into each arraylist
+  for (int ii = 0; ii <= pmax[0]; ii++){
+      int tuple[] = {ii};
+      dmap[ii]->addEntry(tuple);
+    }
+  }
+
+  int ctr = 0;
+  for (int k = 0; k < num_total_degrees; k++){    
+    int nrecords = dmap[k]->getNumEntries();
+    dmap[k]->getEntries(&indx[ctr]);
+    ctr = ctr + nrecords;
+  }  
+  nindices[0] = ctr;
 }
 
 void BasisHelper::bivariateBasisDegrees(const int nvars, const int *pmax, 
@@ -87,38 +118,86 @@ void BasisHelper::bivariateBasisDegrees(const int nvars, const int *pmax,
     ctr = ctr + nrecords;
   }  
   nindices[0] = ctr;
-
-  /*
-  ArrayList **degree_list = NULL;
-  for (int k = 0; k < num_total_degrees; k++){
-    degree_list[k] = new ArrayList(nterms, nvars);
-  }
-
-  return;
-  for (int ii = 0; ii < pmax[0]; ii++){
-    for (int jj = 0; jj < pmax[1]; jj++){
-      printf("adding %d %d \n", ii, jj);
-      int tuple[] = {ii,jj};
-      degree_list[ii+jj]->addEntry(tuple);
-    }
-  }
-  
-  // Flatten list and return into indx
-  int ctr = 0;
-  for (int k = 0; k < num_total_degrees; k++){    
-    int nrecords = degree_list[k]->getNumEntries();
-    degree_[k]->getEntries(&indx[ctr]);
-    ctr = ctr + nrecords;
-  }  
-  nindices[0] = ctr;
-  */
-  
 }
 
 void BasisHelper::trivariateBasisDegrees(const int nvars, const int *pmax, 
-                                         int *nindices, int **indx){}
+                                         int *nindices, int **indx){
+  // Number of terms from tensor product
+  int nterms = 1;
+  for (int i = 0; i < nvars; i++){
+    nterms *= 1 + pmax[i];
+  }
+
+  int num_total_degrees = 1;
+  for (int i = 0; i < nvars; i++){
+    num_total_degrees += pmax[i];
+  }
+
+  // Create a map of empty array lists
+  std::map<int,ArrayList*> dmap;
+  for (int k = 0; k < num_total_degrees; k++){
+    dmap.insert(pair<int, ArrayList*>(k, new ArrayList(nterms, nvars)));
+  }
+
+  // Add degree wise tuples into each arraylist
+  for (int ii = 0; ii <= pmax[0]; ii++){
+    for (int jj = 0; jj <= pmax[1]; jj++){
+      for (int kk = 0; kk <= pmax[2]; kk++){
+        int tuple[] = {ii,jj,kk};
+        dmap[ii+jj+kk]->addEntry(tuple);
+      }
+    }
+  }
+
+  int ctr = 0;
+  for (int k = 0; k < num_total_degrees; k++){    
+    int nrecords = dmap[k]->getNumEntries();
+    dmap[k]->getEntries(&indx[ctr]);
+    ctr = ctr + nrecords;
+  }  
+  nindices[0] = ctr;
+}
+
 void BasisHelper::quadvariateBasisDegrees(const int nvars, const int *pmax, 
-                                          int *nindices, int **indx){}
+                                          int *nindices, int **indx){
+  // Number of terms from tensor product
+  int nterms = 1;
+  for (int i = 0; i < nvars; i++){
+    nterms *= 1 + pmax[i];
+  }
+
+  int num_total_degrees = 1;
+  for (int i = 0; i < nvars; i++){
+    num_total_degrees += pmax[i];
+  }
+
+  // Create a map of empty array lists
+  std::map<int,ArrayList*> dmap;
+  for (int k = 0; k < num_total_degrees; k++){
+    dmap.insert(pair<int, ArrayList*>(k, new ArrayList(nterms, nvars)));
+  }
+
+  // Add degree wise tuples into each arraylist
+  for (int ii = 0; ii <= pmax[0]; ii++){
+    for (int jj = 0; jj <= pmax[1]; jj++){
+      for (int kk = 0; kk <= pmax[2]; kk++){
+        for (int ll = 0; ll <= pmax[3]; ll++){
+          int tuple[] = {ii,jj,kk,ll};
+          dmap[ii+jj+kk+ll]->addEntry(tuple);
+        }
+      }
+    }
+  }
+
+  int ctr = 0;
+  for (int k = 0; k < num_total_degrees; k++){    
+    int nrecords = dmap[k]->getNumEntries();
+    dmap[k]->getEntries(&indx[ctr]);
+    ctr = ctr + nrecords;
+  }  
+  nindices[0] = ctr;
+}
+
 void BasisHelper::pentavariateBasisDegrees(const int nvars, const int *pmax, 
                                            int *nindices, int **indx){
   // Number of terms from tensor product
@@ -159,5 +238,4 @@ void BasisHelper::pentavariateBasisDegrees(const int nvars, const int *pmax,
     ctr = ctr + nrecords;
   }  
   nindices[0] = ctr;
-
 }
