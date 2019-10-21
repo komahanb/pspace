7af948adf46619b6f61e8db1c13cf1b19a654362
Komahan Boopathy <komahan.cool@gmail.com>
2019-10-21 11:32:15 -0400

    changes for python3 compatibility

--- cpp/setup.py
+++ cpp/setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 setup(ext_modules = cythonize(Extension(
-           "corthogonal_polynomials",                                # the extesion name
-           sources=["corthogonal_polynomials.pyx", "OrthogonalPolynomials.cpp"], # the Cython source and                                                  # additional C++ source files
-           language="c++",                        # generate and compile C++ code
+           "orthogonal_polynomials", # the extesion name
+           sources=["orthogonal_polynomials.pyx", "OrthogonalPolynomials.cpp"], # the Cython source and # additional C++ source files
+           language="c++", # generate and compile C++ code
       )))
