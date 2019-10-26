be635b7f08de418e8d6e1788a3dc8977d1c64837
Komahan Boopathy <komahan.cool@gmail.com>
2019-10-26 02:15:15 -0400

    added setters for function pointers and updated factory

--- cpp/AbstractParameter.h
+++ cpp/AbstractParameter.h
@@ -14,32 +14,22 @@ class AbstractParameter {
   virtual void quadrature(int npoints, double *z, double *y, double *w) = 0;
   virtual double basis(double z, int d) = 0;
 
-  // Defer this to subclass??
-  /* virtual update(double value) = 0; */
-  /* virtual double fetch() = 0; */
-
   // Implemented procedures
   int getParameterID();  
   void setParameterID(int pid);
 
-  // Make this a part of TACS maybe?
-  // Fancy test stuff
-  void setClient(void* obj){
-    this->elem = obj;
-  }
+  // Function pointer to set values
   void setClientFunction( void (*func)(void*, double) ){
     this->set = func;
   }
 
-  void *elem; // points to element of consitituvive object to update 
+  // Function pointer to retrieve values
+  void getClientFunction( double (*func)(void*) ){
+    this->get = func;
+  }
+
   void (*set)(void*, double); // handle to set things
   double (*get)(void*);   // handle to get things
-  void updateValue(double value){
-    this->set(this->elem, value);
-  };
-  double getValue(){
-    return this->get(this->elem);
-  };
 
  protected:
   GaussianQuadrature *gauss;
