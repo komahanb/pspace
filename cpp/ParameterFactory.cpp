c58738e3f8ef9cd91623e520274f8e8b2e93674a
Komahan Boopathy <komahan.cool@gmail.com>
2019-10-26 02:15:15 -0400

    added setters for function pointers and updated factory

--- cpp/ParameterFactory.cpp
+++ cpp/ParameterFactory.cpp
@@ -3,20 +3,62 @@
 ParameterFactory::ParameterFactory(){}
 ParameterFactory::~ParameterFactory(){}
 
-AbstractParameter* ParameterFactory::createNormalParameter(double mu, double sigma){
+AbstractParameter* ParameterFactory::createNormalParameter(double mu, double sigma)
+{
   int pid = this->next_parameter_id;
   this->next_parameter_id++;
   return (AbstractParameter*) new NormalParameter(pid, mu, sigma);  
 }
 
-AbstractParameter* ParameterFactory::createUniformParameter(double a, double b){
+AbstractParameter* ParameterFactory::createUniformParameter(double a, double b)
+{
   int pid = this->next_parameter_id;
   this->next_parameter_id++;
   return (AbstractParameter*) new UniformParameter(pid, a, b);  
 }
 
-AbstractParameter* ParameterFactory::createExponentialParameter(double mu, double beta){
+AbstractParameter* ParameterFactory::createExponentialParameter(double mu, double beta)
+{
   int pid = this->next_parameter_id;
   this->next_parameter_id++;
   return (AbstractParameter*) new ExponentialParameter(pid, mu, beta);  
 }
+
+
+AbstractParameter* ParameterFactory::createNormalParameter( double mu, double sigma,
+                                                            void (*setfunc)(void*, double),
+                                                            double (*getfunc)(void*) )
+{
+  int pid = this->next_parameter_id;
+  this->next_parameter_id++;
+  AbstractParameter *param =  (AbstractParameter*) new NormalParameter(pid, mu, sigma); 
+  param->setClientFunction(setfunc);
+  param->getClientFunction(getfunc);
+  return param;
+}
+
+AbstractParameter* ParameterFactory::createUniformParameter( double a, double b,
+                                                             void (*setfunc)(void*, double),
+                                                             double (*getfunc)(void*) )
+{
+  int pid = this->next_parameter_id;
+  this->next_parameter_id++;
+  AbstractParameter *param = (AbstractParameter*) new UniformParameter(pid, a, b);  
+  param->setClientFunction(setfunc);
+  param->getClientFunction(getfunc);
+  return param;
+}
+
+AbstractParameter* ParameterFactory::createExponentialParameter( double mu, double beta,
+                                                                 void (*setfunc)(void*, double),
+                                                                 double (*getfunc)(void*) )
+{
+  int pid = this->next_parameter_id;
+  this->next_parameter_id++;
+  AbstractParameter *param = (AbstractParameter*) new ExponentialParameter(pid, mu, beta);
+  param->setClientFunction(setfunc);
+  param->getClientFunction(getfunc);
+  return param;
+}
+
+
--- cpp/ParameterFactory.h
+++ cpp/ParameterFactory.h
@@ -10,9 +10,20 @@ class ParameterFactory {
   ~ParameterFactory();
   
   // Creates different parameter types
-  AbstractParameter* createNormalParameter(double mu, double sigma);
-  AbstractParameter* createUniformParameter(double a, double b);
-  AbstractParameter* createExponentialParameter(double mu, double);
+  AbstractParameter* createNormalParameter( double mu, double sigma );
+  AbstractParameter* createUniformParameter( double a, double b );
+  AbstractParameter* createExponentialParameter( double mu, double beta );
+
+  // Overloaded constructors
+  AbstractParameter* createNormalParameter( double mu, double sigma,
+                                            void (*setfunc)(void*, double),
+                                            double (*getfunc)(void*) );
+  AbstractParameter* createUniformParameter( double a, double b,
+                                             void (*setfunc)(void*, double),
+                                             double (*getfunc)(void*) );
+  AbstractParameter* createExponentialParameter( double mu, double beta,
+                                                 void (*setfunc)(void*, double),
+                                                 double (*getfunc)(void*) );
   
  private:
   int next_parameter_id;
