f8f0c2e24b78d26acff23293e6af523c5e914da0
Komahan Boopathy <komahan.cool@gmail.com>
2019-11-11 18:10:58 -0500

    update spring element

--- examples/tacs/smd.h
+++ examples/tacs/smd.h
@@ -3,27 +3,30 @@ class SMD : public TACSElement{
  public:
   SMD(double m, double c, double k);
 
-  // Return the Initial conditions
-  // -----------------------------
+  /**
+     Return the Initial conditions
+  */
   void getInitConditions( int elemIndex, const TacsScalar X[],
                           TacsScalar v[], TacsScalar dv[], TacsScalar ddv[] );
 
-  // Compute the residual of the governing equations
-  // -----------------------------------------------
+  /**
+     Compute the residual of the governing equations
+  */
   void addResidual( int elemIndex, double time,
                     const TacsScalar X[], const TacsScalar v[],
                     const TacsScalar dv[], const TacsScalar ddv[],
                     TacsScalar res[] );
 
-  // Compute the Jacobian of the governing equations
-  // -----------------------------------------------
+  /**
+     Compute the Jacobian of the governing equations
+  */
   void addJacobian( int elemIndex, double time,
                     TacsScalar alpha, TacsScalar beta, TacsScalar gamma,
                     const TacsScalar X[], const TacsScalar v[],
                     const TacsScalar dv[], const TacsScalar ddv[],
                     TacsScalar res[], TacsScalar mat[] );  
   /**
-    Evaluate a point-wise quantity of interest.
+     Evaluate a point-wise quantity of interest.
   */
   int evalPointQuantity( int elemIndex, int quantityType, double time,
                          int n, double pt[], const TacsScalar Xpts[],
