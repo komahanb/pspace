6e29407f2967dfcbd9222d34905d17a991454807
komahan <komahan.cool@gmail.com>
2019-12-04 22:59:22 -0500

    added scalar header

---
+++ cpp/scalar.h
@@ -0,0 +1,50 @@
+#ifndef COMPLEX_STEP_H
+#define COMPLEX_STEP_H
+
+#include <stdlib.h>
+#include <complex>
+#include "mpi.h"
+
+/*
+  Use the cplx type 
+*/
+using namespace std;
+
+typedef std::complex<double> Complex;
+typedef double Real;
+
+/*
+  Define the basic scalar type 
+*/
+#ifdef USE_COMPLEX
+#define MPI_TYPE MPI_DOUBLE_COMPLEX
+typedef Complex scalar;
+#else
+#define MPI_TYPE MPI_DOUBLE
+typedef double scalar;
+#endif
+
+// Define the real part function for the complex data type
+inline double RealPart( const std::complex<double>& c ){
+  return real(c);
+}
+
+// Define the imaginary part function for the complex data type
+inline double ImagPart( const std::complex<double>& c ){
+  return imag(c);
+}
+
+// Dummy function for real part
+inline double RealPart( const double& r ){
+  return r;
+}
+
+// Compute the absolute value
+inline std::complex<double> dabs( const std::complex<double>& c ){
+  if (real(c) < 0.0){
+    return -c;
+  }
+  return c;
+}
+
+#endif
