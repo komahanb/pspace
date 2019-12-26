458b229958dfd2762f39f705023dd113957db10a
Komahan Boopathy <komahan.cool@gmail.com>
2019-12-25 22:58:27 -0500

    setup sampling code

--- examples/tacs/fourbar/sampling.cpp
+++ examples/tacs/fourbar/sampling.cpp
@@ -2,131 +2,14 @@
 #include "TACSIntegrator.h"
 #include "TACSRigidBody.h"
 #include "TACSKinematicConstraints.h"
-#include "TACSTimoshenkoConstitutive.h"
+#include "SquareSection.h"
 #include "MITC3.h"
 #include "TACSKSFailure.h"
 #include "TACSStructuralMass.h"
 #include "TACSConstitutiveVerification.h"
 #include "TACSElementVerification.h"
-
 #include "ParameterContainer.h"
 #include "ParameterFactory.h"
-
-class SquareSection : public TACSTimoshenkoConstitutive {
-public:
-  static const double kcorr;
-
-  SquareSection( TacsScalar _density, TacsScalar _E, TacsScalar _G,
-                 TacsScalar _w, int _wNum,
-                 const TacsScalar axis[] ):
-    TACSTimoshenkoConstitutive(NULL, NULL, axis){
-    density = _density;
-    E = _E;
-    G = _G;
-    w = _w;
-    wNum = _wNum;
-    if (wNum < 0){
-      wNum = 0;
-    }
-
-    computeProperties();
-  }
-
-  void computeProperties(){
-    // Set the properties based on the width/thickness variables
-    TacsScalar A = w*w;
-    TacsScalar Iy = w*w*w*w/12.0;
-    TacsScalar Iz = Iy;
-    TacsScalar J = Iy + Iz;
-    TacsScalar Iyz = 0.0;
-
-    // Set the entries of the stiffness matrix
-    memset(C, 0, 36*sizeof(TacsScalar));
-    C[0] = E*A;
-    C[7] = G*J;
-    C[14] = E*Iy;
-    C[21] = E*Iz;
-    C[28] = kcorr*G*A;
-    C[35] = kcorr*G*A;
-
-    // Set the entries of the density matrix
-    rho[0] = density*A;
-    rho[1] = density*Iy;
-    rho[2] = density*Iz;
-    rho[3] = density*Iyz;
-  }
-
-  int getDesignVarNums( int elemIndex, int dvLen, int dvNums[] ){
-    if (dvNums){
-      dvNums[0] = wNum;
-    }
-    return 1;
-  }
-  int setDesignVars( int elemIndex, int dvLen, const TacsScalar dvs[] ){
-    w = dvs[0];
-    computeProperties();
-    return 1;
-  }
-  int getDesignVars( int elemIndex, int dvLen, TacsScalar dvs[] ){
-    dvs[0] = w;
-    return 1;
-  }
-  int getDesignVarRange( int elemIndex, int dvLen,
-                         TacsScalar lb[], TacsScalar ub[] ){
-    lb[0] = 0.0;
-    ub[0] = 10.0;
-    return 1;
-  }
-  void addStressDVSens( int elemIndex, const double pt[], const TacsScalar X[],
-                        const TacsScalar e[], TacsScalar scale,
-                        const TacsScalar psi[], int dvLen, TacsScalar dfdx[] ){
-    dfdx[0] += scale*(2.0*w*(E*e[0]*psi[0] + kcorr*G*(e[4]*psi[4] + e[5]*psi[5])) +
-                      (w*w*w/3.0)*(2.0*G*e[1]*psi[1] + E*(e[2]*psi[2] + e[3]*psi[3])));
-  }
-  void addMassMomentsDVSens( int elemIndex, const double pt[],
-                             TacsScalar scale, const TacsScalar psi[],
-                             int dvLen, TacsScalar dfdx[] ){
-    dfdx[0] += scale*density*(2.0*w*psi[0] + ((w*w*w)/3.0)*(psi[1] + psi[2]));
-  }
-
-  TacsScalar evalDensity( int elemIndex, const double pt[],
-                          const TacsScalar X[] ){
-    return density*w*w;
-  }
-  void addDensityDVSens( int elemIndex, const double pt[],
-                         const TacsScalar X[], const TacsScalar scale,
-                         int dvLen, TacsScalar dfdx[] ){
-    dfdx[0] += 2.0*scale*density*w;
-  }
-
-  TacsScalar evalFailure( int elemIndex, const double pt[],
-                          const TacsScalar X[], const TacsScalar e[] ){
-    return E*w*w*fabs(e[0])/10e3;
-  }
-  TacsScalar evalFailureStrainSens( int elemIndex, const double pt[],
-                                    const TacsScalar X[], const TacsScalar e[],
-                                    TacsScalar sens[] ){
-    memset(sens, 0, 6*sizeof(TacsScalar));
-    if (TacsRealPart(e[0]) >= 0.0){
-      sens[0] = E*w*w/10e3;
-    }
-    else {
-      sens[0] = -E*w*w/10e3;
-    }
-    return E*w*w*fabs(e[0])/10e3;
-  }
-  void addFailureDVSens( int elemIndex, const double pt[],
-                         const TacsScalar X[], const TacsScalar e[],
-                         TacsScalar scale, int dvLen, TacsScalar dfdx[] ){
-    dfdx[0] += 2.0*scale*E*w*fabs(e[0])/10e3;
-  }
-
-  TacsScalar density, E, G;
-  TacsScalar w;
-  int wNum;
-};
-
-const double SquareSection::kcorr = 5.0/6.0;
 
 /*
   Create and return the TACSAssembler object for the four bar
@@ -342,7 +225,7 @@ int main( int argc, char *argv[] ){
   MPI_Init(&argc, &argv);
 
   // The number of total steps (100 per second)
-  const int num_steps = 1200;
+  const int num_steps = 1200;  
   const int num_bars = 3;
   int pnqpts[1] = {15};
   ParameterFactory *factory = new ParameterFactory();
@@ -398,7 +281,8 @@ int main( int argc, char *argv[] ){
     // Set the integrator options
     integrator->setUseSchurMat(1, TACSAssembler::TACS_AMD_ORDER);
     integrator->setAbsTol(1e-7);
-    integrator->setOutputFrequency(10);
+    integrator->setRelTol(1e-12);
+    integrator->setOutputFrequency(0);
 
     // Integrate the equations of motion forward in time
     integrator->integrate();
@@ -406,10 +290,10 @@ int main( int argc, char *argv[] ){
     // Create the continuous KS function
     double ksRho = 100.0;
     TACSKSFailure *ksfunc = new TACSKSFailure(assembler, ksRho);
-    ksfunc->setKSFailureType(TACSKSFailure::DISCRETE);
+    TACSStructuralMass *fmass = new TACSStructuralMass(assembler);
 
     // Set the functions
-    TACSFunction *funcs = ksfunc;
+    TACSFunction *funcs = fmass; // ksfunc
     integrator->setFunctions(1, &funcs);
 
     TacsScalar fval;
