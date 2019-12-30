3c25ef03bcc4e9836d8d48de0133e01975129d6f
Komahan Boopathy <komahan.cool@gmail.com>
2019-12-30 17:01:54 -0500

    setup ouu with samplig and implemented derivative check

---
+++ examples/tacs/fourbar/sampling.h
@@ -0,0 +1,9 @@
+#ifndef DETERMINISTIC_H
+#define DETERMINISTIC_H
+
+#include "TACSAssembler.h"
+#include "ParameterContainer.h"
+
+TACSAssembler* four_bar_mechanism( int nA, int nB, int nC, TacsScalar theta );
+
+#endif
