b371347c980fed140a1414c6ab3c0ddc43666623
Komahan Boopathy <komahan.cool@gmail.com>
2020-01-31 22:34:11 -0500

    wrapped main classes

--- pspace/cwrap.pxd
+++ pspace/cwrap.pxd
@@ -1,8 +1,35 @@
 # Typdefs required for either real or complex mode
 include "PspaceTypedefs.pxi"
 
-cdef extern from "NormalParameter.h":
-    cdef cppclass NormalParameter:
-        NormalParameter(int pid, scalar mu, scalar sigma)
+cdef extern from "AbstractParameter.h":
+    cdef cppclass AbstractParameter:
         #void quadrature(int npoints, scalar *z, scalar *y, scalar *w)
-        #scalar basis(scalar z, int d)
+        scalar basis(scalar z, int d)
+
+cdef extern from "ParameterFactory.h":
+    cdef cppclass ParameterFactory:
+        ParameterFactory()
+        AbstractParameter* createNormalParameter(scalar mu, scalar sigma, int dmax)
+        AbstractParameter* createUniformParameter(scalar a, scalar b, int dmax)
+        AbstractParameter* createExponentialParameter(scalar mu, scalar beta, int dmax)
+
+cdef extern from "ParameterContainer.h":
+    cdef cppclass ParameterContainer:
+        ParameterContainer(int basis_type, int quadrature_type)
+        void addParameter(AbstractParameter *param)
+
+        # Evaluate basis at quadrature points
+        #scalar quadrature(int q, scalar *zq, scalar *yq)
+        #scalar basis(int k, scalar *z)
+
+        # Accessors
+        int getNumBasisTerms()
+        int getNumParameters()
+        int getNumQuadraturePoints()
+        #void getBasisParamDeg(int k, int *degs)
+        #void getBasisParamMaxDeg(int *pmax)
+
+        # Initiliazation tasks
+        void initialize();
+        #void initializeBasis(const int *pmax)
+        #void initializeQuadrature(const int *nqpts)
--- pspace/cwrap.pyx
+++ pspace/cwrap.pyx
@@ -1,12 +1,58 @@
 # distutils: language = c++
-from cwrap cimport NormalParameter
+from cwrap cimport *
 import numpy as np
 
 # Include the definitions
 include "PspaceDefs.pxi"
 
-cdef class PyNormalParameter:
-    cdef NormalParameter *ptr
-    def __cinit__(self, int pid, scalar mu, scalar sigma):
-        self.ptr = new NormalParameter(pid, mu, sigma)
+cdef class PyAbstractParameter:
+    cdef AbstractParameter *ptr
+    def __cinit__(self):
+        self.ptr = NULL
         return
+    def basis(self, scalar z, int d):
+        return self.ptr.basis(z,d)
+
+cdef class PyParameterFactory:
+    cdef ParameterFactory *ptr
+    def __cinit__(self):
+        self.ptr = new ParameterFactory()
+        return
+    def createNormalParameter(self, scalar mu, scalar sigma, int dmax):
+        cdef PyAbstractParameter pyparam = PyAbstractParameter()
+        pyparam.ptr = self.ptr.createNormalParameter(mu, sigma, dmax) 
+        return pyparam
+    def createUniformParameter(self, scalar a, scalar b, int dmax):
+        cdef PyAbstractParameter pyparam = PyAbstractParameter()
+        pyparam.ptr = self.ptr.createUniformParameter(a, b, dmax) 
+        return pyparam
+    def createExponentialParameter(self, scalar mu, scalar beta, int dmax):
+        cdef PyAbstractParameter pyparam = PyAbstractParameter()
+        pyparam.ptr = self.ptr.createExponentialParameter(mu, beta, dmax) 
+        return pyparam
+
+cdef class PyParameterContainer:
+    cdef ParameterContainer *ptr
+    def __cinit__(self, int basis_type=0, int quadrature_type=0):
+        self.ptr = new ParameterContainer(basis_type, quadrature_type)
+        return
+    def addParameter(self, PyAbstractParameter param):
+        self.ptr.addParameter(param.ptr)
+        return
+    def getNumBasisTerms(self):        
+        return self.ptr.getNumBasisTerms()
+    def getNumParameters(self):
+        return self.ptr.getNumParameters()
+    def getNumQuadraturePoints(self):
+        return self.ptr.getNumQuadraturePoints()
+    def initialize(self):
+        self.ptr.initialize()
+        return
+    
+    
+## cdef class PyNormalParameter(PyAbstractParameter):
+##     cdef NormalParameter *dptr
+##     def __cinit__(self, int pid, scalar mu, scalar sigma):
+##         self.dptr = new NormalParameter(pid, mu, sigma)
+##         self.ptr = self.dptr
+##         return
