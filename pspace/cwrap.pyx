a63b2622ef79ed647b3f33e262027d90788763c6
Komahan Boopathy <komahan.cool@gmail.com>
2020-02-02 06:01:33 -0500

    wrapped more functions

--- pspace/cwrap.pyx
+++ pspace/cwrap.pyx
@@ -1,8 +1,10 @@
 # distutils: language = c++
 from cwrap cimport *
+
+# Import numpy
+cimport numpy as np
 import numpy as np
 
-# Include the definitions
 include "PspaceDefs.pxi"
 
 cdef class PyAbstractParameter:
@@ -36,23 +38,50 @@ cdef class PyParameterContainer:
     def __cinit__(self, int basis_type=0, int quadrature_type=0):
         self.ptr = new ParameterContainer(basis_type, quadrature_type)
         return
+
     def addParameter(self, PyAbstractParameter param):
         self.ptr.addParameter(param.ptr)
         return
+    
+    def basis(self, int k, np.ndarray[scalar, ndim=1, mode='c'] z):
+        return self.ptr.basis(k, <scalar*> z.data)    
+    def quadrature(self, int q):
+        nparams = self.getNumParameters()
+        cdef np.ndarray yq = None
+        cdef np.ndarray zq = None
+        yq = np.zeros(nparams, dtype=dtype)
+        zq = np.zeros(nparams, dtype=dtype)
+        self.ptr.quadrature(q, <scalar*> zq.data, <scalar*> yq.data)
+        return zq, yq
+
     def getNumBasisTerms(self):        
         return self.ptr.getNumBasisTerms()
     def getNumParameters(self):
         return self.ptr.getNumParameters()
     def getNumQuadraturePoints(self):
         return self.ptr.getNumQuadraturePoints()
+
+    ## def getBasisParamDeg(self, int k):
+    ##     nparams = self.getNumParameters()
+    ##     cdef np.ndarray degs = None
+    ##     degs = np.zeros(nparams, dtype=int)
+    ##     #self.ptr.getBasisParamDeg(k, <int*> degs.data)
+    ##     return degs    
+    ## def getBasisParamMaxDeg(self):
+    ##     nparams = self.getNumParameters()
+    ##     cdef np.ndarray pmax = None
+    ##     pmax = np.zeros(nparams, dtype=int)
+    ##     self.ptr.getBasisParamMaxDeg(<int*> pmax.data)
+    ##     return pmax
+
     def initialize(self):
         self.ptr.initialize()
         return
+    def initializeBasis(self, np.ndarray[int, ndim=1, mode='c'] pmax):
+        self.ptr.initializeBasis(<int*> pmax.data)
+        return
+    def initializeQuadrature(self, np.ndarray[int, ndim=1, mode='c'] nqpts):
+        self.ptr.initializeQuadrature(<int*> nqpts.data)
+        return
+
     
-    
-## cdef class PyNormalParameter(PyAbstractParameter):
-##     cdef NormalParameter *dptr
-##     def __cinit__(self, int pid, scalar mu, scalar sigma):
-##         self.dptr = new NormalParameter(pid, mu, sigma)
-##         self.ptr = self.dptr
-##         return
