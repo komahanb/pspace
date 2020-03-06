eae4e4f73e4cd9f936bf182e24f95e0def2fc584
Komahan Boopathy <komahan.cool@gmail.com>
2020-03-06 18:00:07 -0500

    removed dependences on tacs, tmr and mpi

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ import sys
 
 # Numpy/mpi4py must be installed prior to installing TACS
 import numpy
-import mpi4py
-import tacs
-import tmr
 
 # Import distutils
 from setuptools import setup
@@ -21,68 +18,35 @@ def get_global_dir(files):
         new.append(os.path.join(tacs_root, f))
     return new
 
-def get_mpi_flags():
-    # Split the output from the mpicxx command
-    args = check_output(['mpicxx', '-show']).decode('utf-8').split()
-
-    # Determine whether the output is an include/link/lib command
-    inc_dirs, lib_dirs, libs = [], [], []
-    for flag in args:
-        if flag[:2] == '-I':
-            inc_dirs.append(flag[2:])
-        elif flag[:2] == '-L':
-            lib_dirs.append(flag[2:])
-        elif flag[:2] == '-l':
-            libs.append(flag[2:])
-
-    return inc_dirs, lib_dirs, libs
-
-inc_dirs, lib_dirs, libs = get_mpi_flags()
+inc_dirs = []
+lib_dirs = []
+libs = []
 
 # Add the numpy/mpi4py directories
-inc_dirs.extend([numpy.get_include(), mpi4py.get_include()])
+inc_dirs.extend([numpy.get_include()])
 
 # PSPACE
 inc_dirs.extend(get_global_dir(['cpp']))
 lib_dirs.extend(get_global_dir(['cpp']))
 
-# TACS
-inc_dirs.extend(tacs.get_include())
-inc_dirs.extend(tacs.get_cython_include())
-lib_dirs.extend(tacs.get_libraries()[0])
-
-# STACS
-inc_dirs.extend(get_global_dir(["examples/stacs/cpp"]))
-lib_dirs.extend(get_global_dir(["examples/stacs/cpp"]))
-
-# TMR
-inc_dirs.extend(tmr.get_include())
-inc_dirs.extend(tmr.get_cython_include())
-lib_dirs.extend(tmr.get_libraries()[0])
-
 # The provide where the run time libraries are present
-runtime_lib_dirs = [] 
+runtime_lib_dirs = []
 runtime_lib_dirs.extend(lib_dirs)
 
-libs.extend(['pspace', 'tacs', 'stacs', 'tmr'])
-
-## print(inc_dirs)
-## print(libs)
-## print(lib_dirs)
-## print(runtime_lib_dirs)
+libs.extend(['pspace'])
 
 exts = []
 for mod in ['PSPACE']:
     exts.append(Ext('pspace.%s'%(mod), sources=['pspace/%s.pyx'%(mod)],
                     include_dirs=inc_dirs,
-                    libraries=libs, 
+                    libraries=libs,
                     library_dirs=lib_dirs,
                     runtime_library_dirs=runtime_lib_dirs,
                     cython_directives={"embedsignature": True, "binding": True}))
 
 setup(name='pspace',
       version=1.0,
-      description='Parallel finite-element analysis package',
+      description='Probabilistic space package for uncertainty quantification and optimization under uncertainty',
       author='Komahan Boopathy',
       author_email='komibuddy@gmail.com',
       ext_modules=cythonize(exts, include_path=inc_dirs))
