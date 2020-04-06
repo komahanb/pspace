62b35d1b1cf6839f7cc98928f20ef3381aa497f9
Komahan Boopathy <komahan.cool@gmail.com>
2020-04-06 14:54:27 -0400

    updated docs

--- src/include/ParameterFactory.h
+++ src/include/ParameterFactory.h
@@ -2,14 +2,19 @@
 #include"NormalParameter.h"
 #include"UniformParameter.h"
 #include"ExponentialParameter.h"
+
 #include "scalar.h"
 
+/**
+   Factory class for creating new probabilistically modeled random
+   parameters
+ */
 class ParameterFactory {
  public:
   // Constructor and Destructor
   ParameterFactory();
   ~ParameterFactory();
-  
+
   // Creates different parameter types
   AbstractParameter* createNormalParameter( scalar mu, scalar sigma );
   AbstractParameter* createUniformParameter( scalar a, scalar b );
@@ -19,8 +24,7 @@ class ParameterFactory {
   AbstractParameter* createNormalParameter(scalar mu, scalar sigma, int dmax);
   AbstractParameter* createUniformParameter(scalar a, scalar b, int dmax);
   AbstractParameter* createExponentialParameter(scalar mu, scalar beta, int dmax);
-  
+
  private:
   int next_parameter_id;
 };
-
