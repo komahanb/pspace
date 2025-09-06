0827f702ce0e09b472f34f14376613ddf93e5e81
Komahan Boopathy <komahan.cool@gmail.com>
2025-09-06 17:07:39 -0400

    moved jacobian formation to the probabilistic domain; adjusted test sparsity to create baseline

--- tests/test_sparsity.py
+++ tests/test_sparsity.py
@@ -1,250 +1,84 @@
+#=====================================================================#
+# File to test jacobian matrix sparsity patterns
+#=====================================================================#
+# Author : Komahan Boopathy (komahan.boopathy@gmail.com)
+#=====================================================================#
+
+# system/os modules
 import sys
+
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
+import os
+outdir  = "tests/baseline"
+os.makedirs(outdir, exist_ok = True)
+
+# third party modules
 import numpy as np
 from collections import Counter
-from timeit import default_timer as timer
 
-from pspace.core import ParameterFactory, ParameterContainer
+# local modules
+from pspace.core import ParameterFactory
+from pspace.core import ParameterContainer
 from pspace.plotter import plot_jacobian
 
-## def sparsity(a, b):
-##     """
-##     Nonzero entries occur when the parameterwise(variablewise) degree
-##     of function 'b' matches or exceeds the degree of the basis vector
-##     'a'.
-##     """
-##     akeys = a.keys()
-##     smap = {}
-##     for akey in akeys:
-##         if b[akey] - a[akey] <= 0:
-##             smap[akey] = True
-##         else:
-##             smap[akey] = False
-##     return smap
-
-def sparse(dmapi, dmapj, dmapf):
-    ## print ''
-    ## print ''
-    ## print 'map i ', dmapi
-    ## print 'map j ', dmapj
-    ## print 'map f ', dmapf
-    smap = {}
-    #print "keys", dmapi.keys()
-    for key in dmapi.keys():
-        #print 'key', key, "diff", abs(dmapi[key] - dmapj[key]), dmapf[key]
-        if abs(dmapi[key] - dmapj[key]) <= dmapf[key]:
-            smap[key] = True
-        else:
-            smap[key] = False
-    return smap
-
-def getSymmetricNonZeroIndices(pc, dmapf):
-    nz = {}
-    N = pc.getNumStochasticBasisTerms()
-    for i in range(N):
-        dmapi = pc.basistermwise_parameter_degrees[i]
-        for j in range(i,N):
-            dmapj = pc.basistermwise_parameter_degrees[j]
-            smap = sparse(dmapi, dmapj, dmapf)
-            if False not in smap.values():
-                dmap = Counter()
-                dmap.update(dmapi)
-                dmap.update(dmapj)
-                dmap.update(dmapf)
-                nqpts_map = pc.getNumQuadraturePointsFromDegree(dmap)
-                nz[(i,j)] = nqpts_map
-    return nz
-
-def getSparseJacobian(pc, f, dmapf):
-    nzs = getSymmetricNonZeroIndices(pc, dmapf)
-    N = pc.getNumStochasticBasisTerms()
-    A = np.zeros((N, N))
-    for index, nqpts in nzs.items():
-        pc.initializeQuadrature(nqpts)
-        pids = pc.getParameters().keys()
-        i = index[0]
-        j = index[1]
-        for q in pc.quadrature_map.keys():
-            val = w(q)*psiz(i,q)*psiz(j,q)*f(q)
-            A[i, j] += val
-            A[j, i] += val
-    return A
-
-def getJacobian(f, dmapf):
-    """
-    """
-    # Test getting ND quadrature points
-    N = pc.getNumStochasticBasisTerms()
-    A = np.zeros((N, N))
-
-    for i in range(N):
-        dmapi = pc.basistermwise_parameter_degrees[i]
-
-        for j in range(N):
-            dmapj = pc.basistermwise_parameter_degrees[j]
-
-            dmap = Counter()
-            dmap.update(dmapi)
-            dmap.update(dmapj)
-            dmap.update(dmapf)
-
-            # add up the degree of both participating functions psizi
-            # and psizj to determine the total degree of integrand
-            nqpts_map = pc.getNumQuadraturePointsFromDegree(dmap)
-            pc.initializeQuadrature(nqpts_map)
-            # print dmap, nqpts_map
-
-            # Loop quadrature points
-            pids = pc.getParameters().keys()
-            for q in pc.quadrature_map.keys():
-                A[i,j] += w(q)*psiz(i,q)*psiz(j,q)*f(q)
-    return A
-
 if __name__ == '__main__':
-    """
-    """
-    start = timer()
-
-    # Create "Parameter" using "Parameter Factory" object
+    # Domain Definition(ADAPTIVE, FIXED={TENSOR, COMPLETE})
     pfactory = ParameterFactory()
-    c = pfactory.createNormalParameter('c', dict(mu=-4.0, sigma=0.50), 4)
-    k = pfactory.createExponentialParameter('k', dict(mu=6.0, beta=1.0), 5)
-    m = pfactory.createUniformParameter('m', dict(a=-5.0, b=4.0), 6)
-    d = pfactory.createUniformParameter('d', dict(a=-5.0, b=4.0), 3)
 
-    # Add "Parameter" into "ParameterContainer"
+    # With adaptive enrichment we can keep the complexity (basis set
+    # size) tied to the intrinsic structure of the function to be
+    # decomposed in the probabilistic domain, not the worst-case
+    # degree cutoffs like 4, 5, 6
+
+    y1 = pfactory.createNormalParameter     ('y1', dict(mu = -4.0, sigma = 0.5), 3)
+    y2 = pfactory.createExponentialParameter('y2', dict(mu = +6.0, beta  = 1.0), 3)
+    y3 = pfactory.createUniformParameter    ('y3', dict(a  = -5.0, b     = 4.0), 3)
+
+    # Add "Parameter" into "ParameterContainer: create Axes in the Domain
     pc = ParameterContainer()
-    pc.addParameter(c)
-    pc.addParameter(k)
-    pc.addParameter(m)
-    pc.addParameter(d)
+
+    pc.addParameter(y1)
+    pc.addParameter(y2)
+    pc.addParameter(y3)
 
     pc.initialize()
 
-    suffix = 'eee'
+    class Function:
+        def __init__(self, func, dmap, name):
+            self.func = func
+            self.name = name
+            self.dmap = dmap
+            return
 
-    # rename for readability
-    w    = lambda q    : pc.W(q)
-    psiz = lambda i, q : pc.evalOrthoNormalBasis(i,q)
-
-    def fy(q):
+    # find a better way to do this
+    def y(q, degree):
         ymap = pc.Y(q)
-        paramids = ymap.keys()
-        ans = 1.0
-        for paramid in paramids:
-            ans = ans*ymap[paramid]
-        return ans
+        return ymap[degree]
 
-    def gy(q,pid):
-        ymap = pc.Y(q)
-        return ymap[pid]
+    def generate_baseline(F : Function):
+        A = pc.getJacobian(F.func, F.dmap)
+        np.save('tests/baseline/matrix-full-assembly-' + F.name + '-.npy', A)
 
-    # Default map for number of quadrature points
+        A = pc.getSparseJacobian(func, dmap)
+        np.save('tests/baseline/matrix-sparse-assembly-' + F.name + '-.npy', A)
+
+    # constant: y0^0 * y1^0 * y2^0
+    func = lambda q : y(q,0) * y(q,1) * y(q,2)
     dmap = Counter()
+    dmap[0] = 0; dmap[1] = 0; dmap[2] = 0;
+    constant_function = Function(func, dmap, "y1^0y2^0y3^0")
 
-    #  y_1^4
-    func = lambda q :  gy(q,1)*gy(q,1)*gy(q,1)*gy(q,1)
-    dmap[0] = 0; dmap[1] = 4; dmap[2] = 0
-    A = getSparseJacobian(pc, func, dmap)
-    #A = getJacobian(func, dmap)
-    plot_jacobian(A, 'sparsity-y3^4-' + suffix + '.pdf')
-
-    # Nonzero constant
-    func = lambda q : 1.0
-    dmap[0] = 0 ;  dmap[1] = 0; dmap[2] = 0
-    #A = getJacobian(func, dmap)
-    A = getSparseJacobian(pc, func, dmap)
-    plot_jacobian(A, 'sparsity-identity-' + suffix + '.pdf')
-
-    # Linear in y_1
-    func = lambda q : gy(q,0)
-    dmap[0] = 1; dmap[1] = 0; dmap[2] = 0
-    A = getSparseJacobian(pc, func, dmap)
-    #A = getJacobian(func, dmap)
-    plot_jacobian(A, 'sparsity-y1-' + suffix + '.pdf')
-
-    # Linear in y_2
-    func = lambda q : gy(q,1)
-    dmap[0] = 0; dmap[1] = 1; dmap[2] = 0
-    A = getSparseJacobian(pc, func, dmap)
-    #A = getJacobian(func, dmap)
-    plot_jacobian(A, 'sparsity-y2-' + suffix + '.pdf')
-
-    # Linear in y_3
-    func = lambda q : gy(q,2)
-    dmap[0] = 0; dmap[1] = 0; dmap[2] = 1
-    A = getSparseJacobian(pc, func, dmap)
-    #A = getJacobian(func, dmap)
-    plot_jacobian(A, 'sparsity-y3-' + suffix + '.pdf')
-
-    # y_1 +  y_2 + y_3
-    func = lambda q : gy(q,0) + gy(q,1) + gy(q,2)
+    # linear : define: y0^1 + y1^1 + y2^1
+    dmap = Counter()
     dmap[0] = 1; dmap[1] = 1; dmap[2] = 1
-    A = getSparseJacobian(pc, func, dmap)
-    #A = getJacobian(func, dmap)
-    plot_jacobian(A, 'sparsity-y1+y2+y3-' + suffix + '.pdf')
+    func = lambda q : y(q,0) + y(q,1) + y(q,2)
+    linear_function = Function(func, dmap, "y1^1+y2^1+y3^1")
 
-    # y_1 *  y_2
-    func = lambda q : gy(q,0) * gy(q,1)
-    dmap[0] = 1; dmap[1] = 1; dmap[2] = 0
-    A = getSparseJacobian(pc, func, dmap)
-    #A = getJacobian(func, dmap)
-    plot_jacobian(A, 'sparsity-y1y2-' + suffix + '.pdf')
-
-    # y_2 *  y_3
-    func = lambda q : gy(q,1) * gy(q,2)
-    dmap[0] = 0; dmap[1] = 1; dmap[2] = 1
-    #A = getJacobian(func, dmap)
-    A = getSparseJacobian(pc, func, dmap)
-    plot_jacobian(A, 'sparsity-y2y3-' + suffix + '.pdf')
-
-    # y_1 *  y_3
-    func = lambda q : gy(q,0) * gy(q,2)
-    dmap[0] = 1; dmap[1] = 0; dmap[2] = 1
-    #A = getJacobian(func, dmap)
-    A = getSparseJacobian(pc, func, dmap)
-    plot_jacobian(A, 'sparsity-y1y3-' + suffix + '.pdf')
-
-    #  y_1^2
-    func = lambda q : gy(q,0)*gy(q,0)
-    dmap[0] = 2; dmap[1] = 0; dmap[2] = 0
-    #A = getJacobian(func, dmap)
-    A = getSparseJacobian(pc, func, dmap)
-    plot_jacobian(A, 'sparsity-y1^2-' + suffix + '.pdf')
-
-    #  y_2^2
-    func = lambda q : gy(q,1)*gy(q,1)
-    dmap[0] = 0; dmap[1] = 2; dmap[2] = 0
-    #A = getJacobian(func, dmap)
-    A = getSparseJacobian(pc, func, dmap)
-    plot_jacobian(A, 'sparsity-y2^2-' + suffix + '.pdf')
-
-    #  y_3^2
-    func = lambda q : gy(q,2)*gy(q,2)
-    dmap[0] = 0; dmap[1] = 0; dmap[2] = 2
-    A = getSparseJacobian(pc, func, dmap)
-    #A = getJacobian(func, dmap)
-    plot_jacobian(A, 'sparsity-y3^2-' + suffix + '.pdf')
-
-    #  y_3^4
-    func = lambda q : gy(q,2)*gy(q,2)*gy(q,2) + gy(q,2)*gy(q,2)*gy(q,2)*gy(q,2)
-    dmap[0] = 0; dmap[1] = 0; dmap[2] = 4
-    A = getSparseJacobian(pc, func, dmap)
-    #A = getJacobian(func, dmap)
-    plot_jacobian(A, 'sparsity-y3^4-' + suffix + '.pdf')
-
-    # y_1 * y_2 * y_3*y_4
-    func = lambda q : gy(q,0) * gy(q,1) * gy(q,2) * gy(q,3)
-    dmap[0] = 1; dmap[1] = 1; dmap[2] = 1; dmap[3] = 1
-    print('first')
-    A = getSparseJacobian(pc, func, dmap)
-    print('second')
-    A = getSparseJacobian(pc, func, dmap)
-    print('third')
-    A = getSparseJacobian(pc, func, dmap)
-    #A = getJacobian(func, dmap)
-    plot_jacobian(A, 'sparsity-y1y2y3y4-' + suffix + '.pdf')
-    end = timer()
-    print("elapsed time:", end - start)
+    # quadratic : define: y0^2 + y1^2 + y2^2
+    dmap = Counter()
+    dmap[0] = 2; dmap[1] = 2; dmap[2] = 2
+    func = lambda q : y(q,0) + y(q,1) + y(q,2)
+    quadratic_function = Function(func, dmap, "y1^2+y2^2+y3^2")
+    generate_baseline(quadratic_function)
