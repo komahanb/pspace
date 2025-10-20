94f7635079eea48e172e6ab64f44bae8ee0939a5
Komahan Boopathy <komahan.cool@gmail.com>
2025-10-20 13:22:05 -0400

    Fix sparsity mask for total-degree bases

--- pspace/core.py
+++ pspace/core.py
@@ -532,18 +532,13 @@ class CoordinateSystem:
         """
         Detect sparsity for <f, ψ_i>.
 
-        dmapi     : Counter({axis: degree}) for basis ψ_i
-        dmapf     : Counter({axis: degree}) for function f
-        basis_type: "tensor" or "total"
+        dmapi : Counter({axis: degree}) for basis ψ_i
+        dmapf : Counter({axis: degree}) for function f
         """
-        if self.basis_construction == BasisFunctionType.TENSOR_DEGREE:
-            # Axis-by-axis cutoff
-            return all(dmapi[k] <= dmapf[k] for k in dmapf.keys())
-        elif self.basis_construction == BasisFunctionType.TOTAL_DEGREE:
-            # Global cutoff (total degree)
-            return sum(dmapi.values()) <= sum(dmapf.values())
-        else:
-            raise ValueError("Unknown basis_type")
+        for axis, deg in dmapi.items():
+            if deg > dmapf.get(axis, 0):
+                return False
+        return True
 
     def monomial_vector_sparsity_mask(self, f_deg: Counter):
         mask = set()
